d1c6801f0444e9718d56b1d9209bf65f141cca26
Dhiraj Dubey <dhiraj.dubey@landisgyr.com>
2018-06-27 15:43:07 +0530

    EmploymentData with Codefirst Approach
    Identification:001
    Change-Id: Iaebc594ac125145f7fc4508201fe760308089fa7

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NewCandidatePortal.Models;
+
+namespace NewCandidatePortal.Controllers
+{
+    public class HomeController : Controller
+    {
+        EmploymentContext objDb = new EmploymentContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ViewResult CreateEmploymentData()
+        {            
+            return View();
+        }
+        [HttpPost]
+        public ViewResult CreateEmploymentData(EmploymentData EmpData)
+        {
+            try
+            {
+                objDb.EmploymentDatas.Add(EmpData);
+                objDb.SaveChanges();
+                return View("success");
+            }
+            catch(Exception ex)
+            {
+                ViewBag.Error = ex.ToString(); ;            
+                return View("error");
+                throw;
+            }
+            
+
+        }
+
+    }
+}
---
+++ Models/EmploymentData.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NewCandidatePortal.Models
+{
+    public class EmploymentData
+    {
+        [Key]
+        public int EmploymentDataId { get; set; }
+        [Required]
+        [MaxLength(40)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(40)]
+        public string LastName { get; set; }
+        [MaxLength(40)]
+        public string FullMiddleName { get; set; }
+        [MaxLength(40)]
+        public string OtherName { get; set; }
+        [Required]
+        [MaxLength(80)]
+        public string Address { get; set; }
+        [Required]
+        [MaxLength(10)]
+        public string AptNumber { get; set; }
+        [Required]
+        [MaxLength(5)]
+        public string ZipCode { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public DateTime DOB { get; set; }
+        [Required]
+        [MaxLength(11)]
+        public string SSN { get; set; }
+        [Required]
+        [EmailAddress]
+        public string PrimaryEmail { get; set; }
+        [EmailAddress]
+        public string SecondaryEmail { get; set; }
+        [Required]
+        [Phone]
+        public string PrimaryPhone { get; set; }
+        [Required]
+        [Phone]
+        public string SecondaryPhone { get; set; }
+        [Required]
+        public string EmergencyContactName { get; set; }
+        [Required]
+        [Phone]
+        public string EmergencyContactPhone { get; set; }
+        [Required]
+        public bool IsSigned { get; set; }
+        [Required]
+        public string SignatureFont { get; set; }
+    }
+}
